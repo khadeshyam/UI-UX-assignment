ebf1bcd0b212956012f42075b0549ef12be981e9
SHYAM KHADE <khade.shyam10.1@gmail.com>
2024-02-07 14:34:11 +0530

    Completed the UI/UX assignment.

---
+++ 01.bash
@@ -0,0 +1,8 @@
+# Add all changes to the staging area
+git add .
+
+# Commit the changes with a descriptive message
+git commit -m "Completed the UI/UX assignment."
+
+# Push the changes to the remote repository (GitHub)
+gh repo view khadeshyam/UI-UX-assignment
